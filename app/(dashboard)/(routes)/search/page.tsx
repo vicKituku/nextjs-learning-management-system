77ff5d46a78cda2eadf37cb64ff14599b4097584
Victor Kituku <victorkituku@gmail.com>
2024-12-17 13:03:00 +0300

    initail commit

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,5 @@
+const SearchPage = () => {
+  return <div>This is a search Page</div>;
+};
+
+export default SearchPage;
---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export const Logo = () => {
+  return <Image width={50} height={50} src="/logo.svg" alt="logo" />;
+};
